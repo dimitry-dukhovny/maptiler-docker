83d84128901e5a9ca8c7ae5128b1f75828670e6b
dimitry-dukhovny <49175877+dimitry-dukhovny@users.noreply.github.com>
2022-02-11 07:02:36 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,16 +22,28 @@ getent group docker > /dev/null 2>&1 || nope "Could not create the docker group.
 useradd -d /home/svc.map -m -s /bin/bash -G docker svc.map 2> /dev/null
 getent passwd svc.map > /dev/null 2>&1 || nope "Could not create the svc.map user.  Are you root?"
 
+# Where our stuff goes
 mkdir -m 775 -p /data/map/bin
+
+# Docker prep
 cp -f ${mydir}/docker-compose.yml /data/map/bin/
 cp -f ${mydir}/docker-maptiler.service /usr/lib/systemd/system/
+
+# Stunnel prep; stupidly assumes your first cert entry is your default
+assumedcert=`/bin/ls /etc/letsencrypt/live | head -1`
+[ ! -d /etc/letsencrypt/live/default ] && ln -s ${assumedcert} /etc/letsencrypt/live/default
 cp -f ${mydir}/stunnel.service /usr/lib/systemd/system/
 [ -f /etc/stunnel/stunnel.conf ] && mv /etc/stunnel/stunnel.conf /etc/stunnel/stunnel.conf.`date +%s`
 cp -f ${mydir}/stunnel.conf /etc/stunnel/
 
+# Selinux context
 chcon -u system_u -r object_r -t system_unit_file_t /usr/lib/systemd/system/docker-maptiler.service /usr/lib/systemd/system/stunnel.service 2> /dev/null
+# Ownership
 chown -R svc.map:docker /data/map || nope "Failed to change ownership of /data/map!  Not continuing!"
 
+# Startup
 systemctl daemon-reload
 systemctl enable stunnel docker-maptiler
 systemctl start stunnel docker-maptiler
+
+echo "Download maps to /data/map and adjust /data/map/bin/docker-compose.yml accordingly"
