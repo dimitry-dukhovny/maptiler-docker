6e9c1b33af67de46486d465632a715f07b8d8a91
dimitry-dukhovny <49175877+dimitry-dukhovny@users.noreply.github.com>
2022-02-11 07:07:26 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,13 +14,16 @@ DOCKERCOMPOSE=`which docker-compose 2> /dev/null`
 DOCKERCOMPOSE=${DOCKERCOMPOSE:-NONE}
 STUNNEL=`which stunnel 2> /dev/null`
 STUNNEL=${STUNNEL:-NONE}
-[ "${DOCKER}" = "NONE" -o "${DOCKERCOMPOSE}" = "NONE" ] && nope "Install docker and docker-compose before resuming."
+[ "${DOCKER}" = "NONE" -o "${DOCKERCOMPOSE}" = "NONE" ] && \
+  nope "Install docker and docker-compose before resuming."
 [ "${STUNNEL}" = "NONE" ] && nope "Install stunnel before resuming."
 
 groupadd docker 2> /dev/null
-getent group docker > /dev/null 2>&1 || nope "Could not create the docker group.  Are you root?"
+getent group docker > /dev/null 2>&1 || \
+  nope "Could not create the docker group.  Are you root?"
 useradd -d /home/svc.map -m -s /bin/bash -G docker svc.map 2> /dev/null
-getent passwd svc.map > /dev/null 2>&1 || nope "Could not create the svc.map user.  Are you root?"
+getent passwd svc.map > /dev/null 2>&1 || \
+  nope "Could not create the svc.map user.  Are you root?"
 
 # Where our stuff goes
 mkdir -m 775 -p /data/map/bin
@@ -31,19 +34,26 @@ cp -f ${mydir}/docker-maptiler.service /usr/lib/systemd/system/
 
 # Stunnel prep; stupidly assumes your first cert entry is your default
 assumedcert=`/bin/ls /etc/letsencrypt/live | head -1`
-[ ! -d /etc/letsencrypt/live/default ] && ln -s ${assumedcert} /etc/letsencrypt/live/default
+[ ! -d /etc/letsencrypt/live/default ] && \
+  ln -s ${assumedcert} /etc/letsencrypt/live/default
 cp -f ${mydir}/stunnel.service /usr/lib/systemd/system/
-[ -f /etc/stunnel/stunnel.conf ] && mv /etc/stunnel/stunnel.conf /etc/stunnel/stunnel.conf.`date +%s`
+[ -f /etc/stunnel/stunnel.conf ] && \
+  mv /etc/stunnel/stunnel.conf /etc/stunnel/stunnel.conf.`date +%s`
 cp -f ${mydir}/stunnel.conf /etc/stunnel/
 
 # Selinux context
-chcon -u system_u -r object_r -t system_unit_file_t /usr/lib/systemd/system/docker-maptiler.service /usr/lib/systemd/system/stunnel.service 2> /dev/null
+chcon -u system_u -r object_r -t system_unit_file_t \
+  /usr/lib/systemd/system/docker-maptiler.service \
+  /usr/lib/systemd/system/stunnel.service \
+  2> /dev/null
 # Ownership
-chown -R svc.map:docker /data/map || nope "Failed to change ownership of /data/map!  Not continuing!"
+chown -R svc.map:docker /data/map || \
+  nope "Failed to change ownership of /data/map!  Not continuing!"
 
 # Startup
 systemctl daemon-reload
 systemctl enable stunnel docker-maptiler
 systemctl start stunnel docker-maptiler
 
-echo "Download maps to /data/map and adjust /data/map/bin/docker-compose.yml accordingly"
+echo "Download maps to /data/map."
+echo "Adjust /data/map/bin/docker-compose.yml accordingly!"
